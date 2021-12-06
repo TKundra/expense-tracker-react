47bc20da09298d23a8190f248b108d2366483402
Tarun <tk@gmail.com>
2021-12-06 15:29:51 +0530

    app id added in .env

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from './App';
 import { Provider } from './context/context';
 import { SpeechProvider } from '@speechly/react-client'
 
+const appID = process.env.REACT_APP_SPPECHLY_APP_KEY;
 ReactDOM.render(
   // <React.StrictMode>
-  <SpeechProvider appId="a0166699-ba25-48d4-aaf0-8cfc8e8c04c0" language="en-US">
+  <SpeechProvider appId={appID} language="en-US">
     <Provider>
       <App />
     </Provider>
